96b1c8f7cf3144922d508b3909223ae514052fae
Kazuki Maeda <kzk_maeda@KazukinoMacBook-Pro.local>
2020-07-28 09:45:26 +0900

    event creation page is created

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,20 @@
 class EventsController < ApplicationController
     def new
-    
+        @event = current_user.created_events.build
     end
+
+    def create
+        @event = current_user.created_events.build(event_params)
+
+        if @event.save
+            redirect_to @event, notice: 'Event has been created.'
+        end
+    end
+
+    private
+        def event_params
+            params.require(:event).permit(
+                :name, :place, :content, :start_at, :end_at
+            )
+        end
 end
