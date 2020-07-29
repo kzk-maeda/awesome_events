812f5fad529d47940356bb9efa33f15018babf52
Kazuki Maeda <kzk_maeda@KazukinoMacBook-Pro.local>
2020-07-29 22:22:18 +0900

    apply event

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/auth/:provider/callback' => 'sessions#create'
   delete '/logout' => 'sessions#destroy'
+
+  resources :events do
+    resources :tickets
+  end
 end
